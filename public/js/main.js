301089831d731b19c42893b3cba9c347337715a1
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2019-03-14 20:31:46 +0000

    listen from file #1

--- public/js/main.js
+++ public/js/main.js
@@ -9,34 +9,46 @@ function preload() {
   sound = loadSound('assets/star-wars.mp3');
 }
 
+
 function setup() {
   var cnv = createCanvas(500,500);
-  //cnv.mouseClicked(togglePlay);
+  cnv.mouseClicked(togglePlay);
 
   fft = new p5.FFT(0.9, 1024);  // the largest value
-  mic = new p5.AudioIn();
-  mic.start();
-  fft.setInput(mic);
+
+  // MIC
+  // mic = new p5.AudioIn();
+  // mic.start();
+  // fft.setInput(mic);
 }
 
 function draw() {
-  let micLevel = mic.getLevel() * 100 //multiply by 100 to work with normal numbers instead of decimal
+  //let micLevel = mic.getLevel() * 100 //multiply by 100 to work with normal numbers instead of decimal
   spectrum = fft.analyze(); // A sound spectrum displays the different frequencies present in a sound.
   var waveform = fft.waveform();
   var sample = sampleRate();
 
-  //filter out some background noise
-  if (micLevel > 1.2) {
-    let frequency = getLoudestFrequency(spectrum);
-    let midi = freqToMidi(frequency)
+  let frequency = getLoudestFrequency(spectrum);
+  let midi = freqToMidi(frequency)
 
-    if (frequency) {
-      //console.log('midi ' + midi);  // midi value
-      console.log('piano ', midi-20); // piano key
-    }
+   if (sound.isPlaying()) {
+    console.log('midi ' + midi);  // midi value
+    console.log('piano ', midi-20); // piano key
   }
 
+  //filter out some background noise
+  // if (micLevel > 1.2) {
+  //   let frequency = getLoudestFrequency(spectrum);
+  //   let midi = freqToMidi(frequency)
+
+  //   if (frequency) {
+  //     //console.log('midi ' + midi);  // midi value
+  //     console.log('piano ', midi-20); // piano key
+  //   }
+  // }
+
 }
+
 
 function getLoudestFrequency(spectrum) {
   var nyquist = sampleRate() / 2; // 22050
@@ -54,6 +66,15 @@ function getLoudestFrequency(spectrum) {
 
   var loudestFreq = largestBin * (nyquist / numberOfBins);
   return loudestFreq;
+}
+
+// fade sound if mouse is over canvas
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.play();
+  }
 }
 
 
