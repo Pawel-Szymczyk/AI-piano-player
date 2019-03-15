6b1580e126b63e50082e6c4ae61b194c7cff257e
Konrad Kolpak <kolpak.conrad@gmail.com>
2019-03-15 12:21:01 +0000

    fitness factor

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,36 +1,47 @@
 
 class Player {
 
-  constructor() {
-      this.notes = ["E6","Eb5","E6","Eb5","E6","B4","D5","C5", "A4"]
-      this.synth = new Tone.Synth().toMaster();
-      this.piano =  SampleLibrary.load({
-        instruments: "piano"
-      });
+  constructor(piano) {
+      this.notes = []
+      this.piano = piano
+      this.fitnessFactor = 0; //the lower the better
+      this.generateNotes()
   }
 
-  setNotesToPlay(notes) {
-    this.notes = notes
+  generateNotes() {
+    this.notes = []
+    for (let i = 0; i < targetNotes.length; i++) {
+      this.notes.push(Tonal.Note.fromMidi(this.getRandomMidiNote()))
+    }
+
+    this.evaluate()
   }
 
+  evaluate() {
+    let totalRange = 0;
+    for (let i = 0; i < this.notes.length; i++) {
+      let range = Tonal.midi(targetNotes[i]) - Tonal.midi(this.notes[i])
 
-  playNote(note,time) {
-    this.synth.triggerAttackRelease(note, time);
+      // console.log(Math.abs(range))
+      totalRange += Math.abs(range);
+    }
+
+    this.fitnessFactor = totalRange / 10 //the lower the closer it was to the target
   }
 
   playNotes() {
     let counter = 0;
     this.notes.forEach((note) => {
-      console.log(note)
       this.piano.toMaster()
-      this.piano.triggerAttackRelease(note, 0.5, counter)
-      counter+=0.2
+      this.piano.triggerAttackRelease(note, 1, counter)
+      counter+=0.3
     })
+  }
 
+  getRandomMidiNote() {
+    return random(69,88)
 
-    // piano.toMaster()
-    // piano.triggerAttack("c3");
-
+    //random(21,108)
   }
 
 }
--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,10 @@ let analyze;
 let spectrum;
 let fft;
 let mic
-let player;
-let keyCount;
-let playback = false
+let players;
+let targetNotes = ["E6","Eb5","E6","Eb5","E6","B4","D5","C5", "A4"] //fur elise
+let piano
+let lifespan = 200;
 
 function preload() {
   // soundFormats('mp3', 'ogg');
@@ -14,68 +15,77 @@ function preload() {
 
 function setup() {
   let cnv = createCanvas(500,500);
-  //cnv.mouseClicked(togglePlay);
-
   fft = new p5.FFT(0.9, 1024);  // the largest value
   mic = new p5.AudioIn();
   mic.start();
   fft.setInput(mic);
-  player = new Player()
-  keyCount = {}
+
+
+  piano =  SampleLibrary.load({
+    instruments: "piano"
+  })
+
+  players = []
+  generatePlayers()
 }
+
 
 function draw() {
   let micLevel = mic.getLevel() * 100 //multiply by 100 to work with normal numbers instead of decimal
   spectrum = fft.analyze(); // A sound spectrum displays the different frequencies present in a sound.
-  var waveform = fft.waveform();
-  var sample = sampleRate();
-
   //filter out some background noise
 
-  if (!playback) {
-    if (micLevel > 1) {
-      let frequency = getLoudestFrequency(spectrum);
-      let midi = freqToMidi(frequency)
-      if (frequency) {
-        // let note = Tonal.Note.fromMidi(midi)
-        // console.log(note)
-        // player.notes.push(note)
-      }
+  if (micLevel > 1) {
+    let frequency = getLoudestFrequency(spectrum);
+    let midi = freqToMidi(frequency)
+    if (frequency) {
+      // let note = Tonal.Note.fromMidi(midi)
+      // console.log(note)
+      // player.notes.push(note)
     }
   }
+
 }
 
 function getLoudestFrequency(spectrum) {
-  var nyquist = sampleRate() / 2; // 22050
-  var numberOfBins = spectrum.length;
-  var maxAmp = 0;
-  var largestBin;
+  const nyquist = sampleRate() / 2; // 22050
+  const numberOfBins = spectrum.length;
+  let maxAmp = 0;
+  let largestBin;
 
-  for (var i = 0; i < numberOfBins; i++) {
-      var thisAmp = spectrum[i]; // amplitude of current bin
+  for (let i = 0; i < numberOfBins; i++) {
+      let thisAmp = spectrum[i]; // amplitude of current bin
       if (thisAmp > maxAmp) {
           maxAmp = thisAmp;
           largestBin = i;
       }
   }
 
-  var loudestFreq = largestBin * (nyquist / numberOfBins);
+  let loudestFreq = largestBin * (nyquist / numberOfBins);
   return loudestFreq;
-}
-
-
-
-
-function mousePressed() {
-  getAudioContext().resume()
 }
 
 
 function keyPressed() {
   if (keyCode == 32) {
-    player.playNotes()
-    playback = true
+    getAudioContext().resume()
+    generatePlayers()
   }
+}
+
+function generatePlayers() {
+    players = []
+    for (let i = 0; i < 200; i++) {
+      players.push(new Player(piano))
+    }
+
+
+  players.forEach((p) => {
+    console.log(p.fitnessFactor)
+    if (p.fitnessFactor < 0.5) {
+      console.log(p)
+    }
+  })
 }
 
 
